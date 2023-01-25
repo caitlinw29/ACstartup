a46be44ac3ea01620c7ea45194d628c7d629abfa
Caitlin Wall <caitlinwall96@gmail.com>
2023-01-25 18:41:09 -0500

    Add login and signup forms and routes

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -13,4 +13,32 @@ router.get("/", async (req, res) => {
   }
 });
 
+//route to render the signup form
+router.get("/signup", async (req, res) => {
+  try {
+    //if already signed in, redirect to homepage
+    if (req.session.logged_in) {
+      res.redirect("/home");
+      return;
+    }
+    res.render("signup");
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+//route to render the login form
+router.get("/login", async (req, res) => {
+  try {
+    //if already logged in, redirect to homepage
+    if (req.session.logged_in) {
+      res.redirect("/home");
+      return;
+    }
+    res.render("login");
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
 module.exports = router;
